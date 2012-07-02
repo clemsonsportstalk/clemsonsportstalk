20e32254c1987d4c0890bc553312501ec703a70f
Lawton Swann <elswann@gmail.com>
2012-07-02 10:33:41 -0400

    Still working on the PAYPAL!!

--- main/store.php
+++ main/store.php
@@ -10,45 +10,37 @@
 		<div class='pam'>
 			<p align="left">Clemson Podcasts is committed to our fans and we are going to give you Clemson themed t-shirts that you can't find anywhere else on the internet. Please support our show by purchasing one or both of these Clemson Podcast originals! We need your help to spread the word about our shows.</p>
 <p align="left">Feel free to contact us at the email address below if you have any interest in being a sponsor of our website or programs. We look forward to creating a relationship with your company that can take your product line to the masses.</p>
-<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIG1QYJKoZIhvcNAQcEoIIGxjCCBsICAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCDAs9vve3cOrr12Ozziod3OSt9OWCz8H3h/EztGSgwrysajtcx4iKRjE4hEZk6QGPrevrDe157ZGkyUOo0hfz7GPe7HKfFyVLce7InwdJNOnSgS0wqO5E28ttfsjNisH1VRVp/VBkeheW7/dL7sLjyDoB/Psqp6Njd4c6d6mb9QzELMAkGBSsOAwIaBQAwUwYJKoZIhvcNAQcBMBQGCCqGSIb3DQMHBAg4nyx68PHqVIAwcZcgQbe9WfRc+PZfvEoUEFvkdEpy5af9j5Vqov/Y0GlT36y+V9P0eKkLF3386KWYoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTIwNjI3MTkwNjE2WjAjBgkqhkiG9w0BCQQxFgQUn30FIGqtCbCLyZJTn+Qwlb5CiyUwDQYJKoZIhvcNAQEBBQAEgYAtfKFceOxZRttAPCQO6RGMm7cTzrSU+nY176gdJ/0wAa3n5wAUJMbvaX8LkF4q7a6+sRgJtwFH5ifJ37fL+x/rpXEESeLamI1itJEs3moOqNfP7XLAKJANkQvcH64IrVifbHSejyJdNj5OiaKo8chVSovrB0OBsGwTBUWFdDHG/g==-----END PKCS7-----
-">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_viewcart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
 
 <p align="left"><img src="../_images/clemsonpodcasts_sales.jpg" width="500" height="375" /></p>
-<p align="left"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+Clemson Podcasts T-Shirt-
+Support our Clemson sports programs with the purchase of a Clemson Podcasts T-Shirt. We appreciate all your support throughout the years. Let Clemson Tiger fans know about our unique programs that bring Clemson sports talk to Tiger fans around the globe.
+<p align="left"><form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="FTUHL9FC2TZ2A">
 <table>
-<tr><td><input type="hidden" name="on0" value="Prices">Prices</td></tr><tr><td><select name="os0">
+<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
 	<option value="Small">Small$15.00 USD</option>
 	<option value="Medium">Medium$15.00 USD</option>
 	<option value="Large">Large$15.00 USD</option>
 	<option value="X-Large">X-Large$15.00 USD</option>
 	<option value="XX-Large">XX-Large$17.00 USD</option>
 </select> </td></tr>
-<tr><td><input type="hidden" name="on1" value="Clemson Podcasts T-Shirt">Clemson Podcasts T-Shirt</td></tr><tr><td><input type="text" name="os1" maxlength="200"></td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIIcQYJKoZIhvcNAQcEoIIIYjCCCF4CAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBKgYzlkY7Bei1YjLPDzopB+mzK64bp1hXJSrVD5jTDf5YEnOCx9oiMFS7ybG1b8rBSi0wViwmjpdCEjwY//ulVxpuAJH1/52zvyBEs9sqGh3JAc4VLmD6BYJ+rjdAXsgBiFFoIRg5wVhfE8g7gFpLl+rwqWEzMnoc+XUZ/VIuiYzELMAkGBSsOAwIaBQAwggHtBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECP7nN61LRqTagIIByPJs8asljlTjtxIl08nQXWSYWDvKd5t3c8gY5pwVdTgYVXa8d8S53BJVN41i6Hok36qVekcP+QqLvgXSQoQOAuCg+TjGb4WPC9SZfBXMTF8cr/8OEGxvfKZJQfM59Cd627Jq7yMy/tamY40G7YO+klorDZhZP+oKjzkwl1KZ4ppU7zaSmtbtJDb47KecQ6zkVuXDSjAyJLRAAOyzeZOjCnNCLUEC2s1rQ4EUKCD5NPSOcnx5EVDKIiOF5jVqVBPAm+c4d1E61QV6ce0tScDG6RVvaftXf9oV7tGAfPQiHFQcqobU661a1DQGbW8iYcd5+Aq78eQIsbyK2Rz+FZKcQT6zSFXHFxh16LBy+QoCXMo0a7+V9u2gcYnxKq+E8VsjEJMsKW8opsgf60OV/Lami0kMuEqppzHk3YtrkDeyHTjAJRJRIE17kebZk4de4Hk7cqIRA5AqNlPAjyZ5BlaD0f1v+LN6RtU2tzlIK7M4s0DU0Z2Uv8IxqN+PoFpKGWCWH+i+ueT6yiyanLw4/QDaoHEMYkkdacnZz4+ajkj83dMWnUVeSVfO1/o7L9u9J/Sk4OtdjqsHnnj+jvwTtaGZdc00eHPNoC+TlKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTEyMDYyODAwMTcxMFowIwYJKoZIhvcNAQkEMRYEFBg/ewEGPRyGunXL/S+NT8fR94KEMA0GCSqGSIb3DQEBAQUABIGAGyXL7e9kC3kIdSWvrjMiF59mEfs7jbez3izlDjm5omIj39Urwurxh6APqSEyWiMWwNuy4NGVXWSeSf1LPfESJTEL6f+nKcpVoBkqqr+xMEqhrB7v7oqu4sNbz81xgcC5nywnDp06tsfdb9FhPD5lc9WcZRl+1Ztzo/gkcwQyIgI=-----END PKCS7-----
-">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
-
 
 
 <p>
 <img src="../_images/pitchfork_sales.jpg" width="500" height="375" /></p>
   <p> The History of the Pitchfork: &quot;They have the Chicken Curse, which was put upon them by Clemson's Ben &quot;Pitchfork&quot; Tillman, because the Legislature of South Carolina didn't want Clemson to exist. Tillman and Thomas Greene Clemson (among others) fought tooth and nail to create a land grant institution for the state's poor farmers. Because of Tillman's frustration over the Legislature's snub, he slammed a pitchfork into the ground of South Carolina's campus and declared them &quot;cursed.&quot;&quot;-ESPN  
         <a href="http://espn.go.com/page2/s/list/readers/worstCollegefb.html">http://espn.go.com/page2/s/list/readers/worstCollegefb.html</a>
-    </p>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    </p><form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
 <input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="9RMBUD458UQHY">
 <table>
-<tr><td><input type="hidden" name="on0" value="Prices">Prices</td></tr><tr><td><select name="os0">
+<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
 	<option value="Small">Small$15.00 USD</option>
 	<option value="Medium">Medium$15.00 USD</option>
 	<option value="Large">Large$15.00 USD</option>
@@ -57,16 +49,14 @@
 </select> </td></tr>
 <tr><td><input type="hidden" name="on1" value="Colors">Colors</td></tr><tr><td><select name="os1">
 	<option value="White">White</option>
-	<option value="Gray">Heather Gray</option>
+	<option value="Heather Gray">Heather Gray</option>
 </select> </td></tr>
-<tr><td><input type="hidden" name="on2" value="Pitchfork Returns T-Shirt">Pitchfork Returns T-Shirt</td></tr><tr><td><input type="text" name="os2" maxlength="200"></td></tr>
 </table>
 <input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIIcQYJKoZIhvcNAQcEoIIIYjCCCF4CAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYB60DpPyfKvQl1YEzDDd3wrw5/lXyQgeM8GyGonfhyxCPuOxisj+qOX/ZFVAu8sVP31/kNFx57ybtksPP1x74mueWSTCaOQkRIOJV5rHP8qDv0IcVQbhfbzzHG/l0xLM+9RV9m1rSSpiXJbPwxQ1sImMWNzP3Kksm7CUOQjZbGxwDELMAkGBSsOAwIaBQAwggHtBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECIYqkcqSRaEngIIByBzJAPElTxhPXJ3KxFplG7cF4ujh9q6DiREnlCq2gsEccwNiz/A2Z1EGfMsJA7xIXZ+uqe482YMKVFmXnADCy4P31ClljPLFVg+q5vXv2dtD49BLHbXbOgN/fJ/Ma+Yhnu1KLO2dUHD7g4iC1lQhqI/lntd1O1dBDqZsDijLXfmRIABwTZyf0Y3jNVF7pqgpLPNLD33bQa+5NNxcT/J+Ie9F4gGZAMe2JZgThr8Ij4vInbwAZwfsMJlRAiWxS++iFY3AwLGSSpypE0fHV+MeL+m923y1IeFaQmDw5wl4yvO4+nZU84LLF83BNQphHmXJNZOPr9nuNgOxjPXT/VQv1YuTBtE8T3DbAB6ypLWkBGu7jqxptjun+yM3U9G1qkvVO+HoH/aAI4Qx3v0RNnuUCJJzSsAUcXkN8bbkjT7XWzTEjmyHSBVm+KC32VY9lSCD8U5vENAuKhPlNjU5fcQa3er6epGPNmmo1aarLPrYxP5JYjckpRcX0aqdZes/rxR84DcYUlmQJXEXm2N1ZIp+/6fSWuDars4akfWnVprETsVS9W2XHUbWU7bKUKFUExj+JPAZ6VtUNuCCJqXaVTHlxM4jUr//5+jka6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTEyMDYyODAwMTg1MFowIwYJKoZIhvcNAQkEMRYEFIyCFO2JoCgy8AhY0GhA0BsBeJYlMA0GCSqGSIb3DQEBAQUABIGAVIFFxMKsdrz2ZxoGCgRWv+5I4a400Pvgx3/lZLoBgH1WvoiKeuEtruW8fJAAoTdfELG3Tkgr9Wh7dNmtwEgeeDE9mFKh1Rg7Tzc+s5JrRxwLZRZCLTIHoryMND7P0j7kqkHM+eUMTESb/E/Fw0fVK7HchTnnx9ynLeOf/JYWCdE=-----END PKCS7-----
-">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 
 <p align="center">  Email: <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a></p>
 			
