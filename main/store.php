aab152a77c005f32b4c6a1a8f4d3833af8337537
Lawton Swann <elswann@gmail.com>
2012-07-02 15:54:26 -0400

    Added new shirts- also removed PayPal until I complete setup

--- main/store.php
+++ main/store.php
@@ -10,53 +10,17 @@
 		<div class='pam'>
 			<p align="left">Clemson Podcasts is committed to our fans and we are going to give you Clemson themed t-shirts that you can't find anywhere else on the internet. Please support our show by purchasing one or both of these Clemson Podcast originals! We need your help to spread the word about our shows.</p>
 <p align="left">Feel free to contact us at the email address below if you have any interest in being a sponsor of our website or programs. We look forward to creating a relationship with your company that can take your product line to the masses.</p>
-
+WE ARE CURRENTLY UPDATING OUR PAYPAL INFORMATION - THE ONLINE STORE WILL REOPEN BY July 2, 2012 at 10PM EST.
 <p align="left"><img src="../_images/clemsonpodcasts_sales.jpg" width="500" height="375" /></p>
-Clemson Podcasts T-Shirt-
-Support our Clemson sports programs with the purchase of a Clemson Podcasts T-Shirt. We appreciate all your support throughout the years. Let Clemson Tiger fans know about our unique programs that bring Clemson sports talk to Tiger fans around the globe.
-<p align="left"><form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="FTUHL9FC2TZ2A">
-<table>
-<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
-	<option value="Small">Small$15.00 USD</option>
-	<option value="Medium">Medium$15.00 USD</option>
-	<option value="Large">Large$15.00 USD</option>
-	<option value="X-Large">X-Large$15.00 USD</option>
-	<option value="XX-Large">XX-Large$17.00 USD</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-
-
+<p>Clemson Podcasts T-Shirt-
+  Support our Clemson sports programs with the purchase of a Clemson Podcasts T-Shirt. We appreciate all your support throughout the years. Let Clemson Tiger fans know about our unique programs that bring Clemson sports talk to Tiger fans around the globe.</p>
+<p><img src="../_images/OnTheProwl_sales.jpg" width="500" height="375" /></p>
 <p>
-<img src="../_images/pitchfork_sales.jpg" width="500" height="375" /></p>
-  <p> The History of the Pitchfork: &quot;They have the Chicken Curse, which was put upon them by Clemson's Ben &quot;Pitchfork&quot; Tillman, because the Legislature of South Carolina didn't want Clemson to exist. Tillman and Thomas Greene Clemson (among others) fought tooth and nail to create a land grant institution for the state's poor farmers. Because of Tillman's frustration over the Legislature's snub, he slammed a pitchfork into the ground of South Carolina's campus and declared them &quot;cursed.&quot;&quot;-ESPN  
+<p><img src="../_images/Rock_Solid_sales.jpg" width="500" height="375" /></p>
+<img src="../_images/pitchfork_sales.jpg" width="500" height="375" />
+<p> The History of the Pitchfork: &quot;They have the Chicken Curse, which was put upon them by Clemson's Ben &quot;Pitchfork&quot; Tillman, because the Legislature of South Carolina didn't want Clemson to exist. Tillman and Thomas Greene Clemson (among others) fought tooth and nail to create a land grant institution for the state's poor farmers. Because of Tillman's frustration over the Legislature's snub, he slammed a pitchfork into the ground of South Carolina's campus and declared them &quot;cursed.&quot;&quot;-ESPN  
         <a href="http://espn.go.com/page2/s/list/readers/worstCollegefb.html">http://espn.go.com/page2/s/list/readers/worstCollegefb.html</a>
-    </p><form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="9RMBUD458UQHY">
-<table>
-<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
-	<option value="Small">Small$15.00 USD</option>
-	<option value="Medium">Medium$15.00 USD</option>
-	<option value="Large">Large$15.00 USD</option>
-	<option value="X-Large">X-Large$15.00 USD</option>
-	<option value="XX-Large">XX-Large$17.00 USD</option>
-</select> </td></tr>
-<tr><td><input type="hidden" name="on1" value="Colors">Colors</td></tr><tr><td><select name="os1">
-	<option value="White">White</option>
-	<option value="Heather Gray">Heather Gray</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="USD">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-
+    </p>
 
 <p align="center">  Email: <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a></p>
 			
