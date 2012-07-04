baa3993a1e5366385e9485840f41ed5e423c6fe3
Lawton Swann <elswann@gmail.com>
2012-07-04 01:34:17 -0400

    Advertising Page and More T-Shirts Added

--- main/ads.php
+++ main/ads.php
@@ -5,12 +5,17 @@
 <?php startblock('main') ?>
 <div class='line'>
 	<div class='unit size4of5 radio'>
-		<h2>Advertisment</h2>
+		<h2>Advertising</h2>
 
 		<div class='pam'>
-			<p align="left"><img src="../_images/heisman_1.jpg" width="200" height="184" align="right">If you are interested in advertising with Clemson Podcasts please let us know. We offer multiple options starting at just $100 per month! Select packages include advertising on www.clemsonpodcasts.com, www.tigernet.com, LIVE on Vokle, the podcast additions, or the radio show heard throughout South Carolina. </p>
-<p align="left">Advertising with Clemson Podcasts can put your company in the eyes and ears of Clemson fans, as well as sports fans around the world. We are one of the most influential Clemson Twitter feeds on the web. Placing an advertisement with Clemson Podcasts will spread your advertisement around the globe.</p>
-<p align="left">Feel free to contact us at the email address below if you have any interest in being a sponsor of our website or programs. We look forward to creating a relationship with your company that can take your product line to the masses.</p>
+			
+<p align="left">If you are interested in advertising with Clemson Podcasts please let us know! We offer multiple packages starting at just $100 per month! Select packages include advertising on www.clemsonpodcasts.com, www.tigernet.com, LIVE on Vokle, the podcast additions, or the radio show heard throughout South Carolina. </p>
+
+<p align="left">Advertising with Clemson Podcasts can put your company in the eyes and ears of Clemson fans, as well as sports fans around the world. We also have one of the most influential Clemson Twitter feeds on the web. Placing an advertisement with Clemson Podcasts will spread your company's advertisement around the globe.</p>
+<p align="left">Feel free to contact us at <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a> if you have any interest in being a sponsor on our website or programs. We look forward to creating a relationship with your company that can take your product line to the masses.
+  </p>
+</p> 
+<div align="center"><img src="../_images/CP_Flyer.gif" width="557" height="721" /></div>
 <p align="center">  Email: <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a></p>
 			<p>
 		</div>
--- main/store.php
+++ main/store.php
@@ -10,14 +10,19 @@
 		<div class='pam'>
 			<p align="left">Clemson Podcasts is committed to our fans and we are going to give you Clemson themed t-shirts that you can't find anywhere else on the internet. Please support our show by purchasing one or both of these Clemson Podcast originals! We need your help to spread the word about our shows.</p>
 <p align="left">Feel free to contact us at the email address below if you have any interest in being a sponsor of our website or programs. We look forward to creating a relationship with your company that can take your product line to the masses.</p>
-WE ARE CURRENTLY UPDATING OUR PAYPAL INFORMATION - THE ONLINE STORE WILL REOPEN BY July 2, 2012 at 10PM EST.
+<strong>WE ARE CURRENTLY UPDATING OUR PAYPAL INFORMATION - THE ONLINE STORE WILL REOPEN BY JULY 4, 2012 at 9PM EST.</strong>
 <p align="left"><img src="../_images/clemsonpodcasts_sales.jpg" width="500" height="375" /></p>
 <p>Clemson Podcasts T-Shirt-
   Support our Clemson sports programs with the purchase of a Clemson Podcasts T-Shirt. We appreciate all your support throughout the years. Let Clemson Tiger fans know about our unique programs that bring Clemson sports talk to Tiger fans around the globe.</p>
 <p><img src="../_images/OnTheProwl_sales.jpg" width="500" height="375" /></p>
-<p>
+<p>Show your Clemson pride with our exclusive &quot;On The Prowl&quot; tee. A tigers eyes are intimidating and strike fear in the hearts of its prey. You can strike that same fear in our opponents on gameday when you purchase this graphic tee.
 <p><img src="../_images/Rock_Solid_sales.jpg" width="500" height="375" /></p>
-<img src="../_images/pitchfork_sales.jpg" width="500" height="375" />
+<p>If your not going to give 110% keep your filth hands of my shirt. Celebrate Clemson's &quot;Rock&quot; Solid tradition wearing this shirt at the tailgate with your crew.</p>
+
+<img src="../_images/1-1-82_Sales_copy.jpg" width="500" height="375" /><p>
+This "Retro" Tee takes you back to the greatest day in Clemson football history. Celebrate the National Title every time you sport one of our 80's tees. </p>
+<img src="../_images/pitchfork_sales_new.jpg" width="500" height="375" />
+<p> On November 24, 2012 Clemson will host South Carolina to put an end to the Gamecocks first three game winning streak in 40 years. Get your pitchfork ready! Celebrate Ben Tillman's pitchfork returning for the 2012 season.
 <p> The History of the Pitchfork: &quot;They have the Chicken Curse, which was put upon them by Clemson's Ben &quot;Pitchfork&quot; Tillman, because the Legislature of South Carolina didn't want Clemson to exist. Tillman and Thomas Greene Clemson (among others) fought tooth and nail to create a land grant institution for the state's poor farmers. Because of Tillman's frustration over the Legislature's snub, he slammed a pitchfork into the ground of South Carolina's campus and declared them &quot;cursed.&quot;&quot;-ESPN  
         <a href="http://espn.go.com/page2/s/list/readers/worstCollegefb.html">http://espn.go.com/page2/s/list/readers/worstCollegefb.html</a>
     </p>
