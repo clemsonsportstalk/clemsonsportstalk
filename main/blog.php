e306a38dc0a0907e737115c9ec105ca9a1d599d0
Lawton Swann <elswann@gmail.com>
2012-07-04 14:48:54 -0400

    Tried to import word press blog with no luck.

--- main/blog.php
+++ main/blog.php
@@ -1,50 +1,31 @@
-<!doctype html>
-<html>
-<head>
-<title>Clemson Podcasts - About</title>
-<link href="/css/common.css" rel="stylesheet" type="text/css">
-<script language="JavaScript" type="text/javascript" src="/js/common.js"></script>
-<?php require_once('../_includes/inc_meta.php'); ?>
-</head>
-<body onLoad="MM_preloadImages('../_images/nav/b_home_f2.jpg','../_images/nav/b_tigernet_talk_f2.jpg','../_images/nav/b_clemson_hotline_f2.jpg','../_images/nav/b_radio_affiliates_f2.jpg','../_images/nav/b_blog_f2.jpg','../_images/nav/b_itunes_f2.jpg','../_images/nav/b_about_f2.jpg')">
-<div class="mainArea">
-
-<div>
-  <table width="955" border="0" cellpadding="0" cellspacing="0" class="mainTable">
-    <tr>
-      <td width="225" rowspan="2" align="left" valign="top" class="bgrdLeft">
-		<?php require_once('../_includes/inc_logo.php'); ?>
-		<?php require_once('../_includes/slideshow.php'); ?>
-		<?php require_once('../_includes/inc_our_shows.php'); ?>
-		<?php require_once('../_includes/inc_our_links.php'); ?>
-		<?php require_once('../_includes/inc_donate.php'); ?></td>
-      <td height="99" colspan="2" align="left" valign="top">
-<div><img src="../_images/header/01.jpg" width="730" height="99"></div>
-<?php require_once('../_includes/inc_nav.php'); ?></td>
-     </tr>
-    <tr>
-      <td width="604" align="left" valign="top" class="bgrdMiddleInternal">
-<div><img src="../_images/titles/about.jpg" alt="About" width="604" height="22"></div>
-<div style="margin:12px 18px 18px 18px; line-height:19px;">
-<div class="podcastTitle"> Clemson Podcasts History</div>
-
-<script type="text/javascript" src="http://output40.rssinclude.com/output?type=js&id=63899&hash=f3b29c8f92a766c1144243654b450367"></script>
+<?php include('base.php') ?>
+<?php startblock('bg') ?>
+<div class='body bg-alt'>
+<?php endblock() ?>
+<?php startblock('main') ?>
 
 
+<div class='line'>
+	<div class='unit size4of5 radio'>
+		<h2>Blog</h2>
 
+		<div class='pam'>
+        
+	
+<p align="left">If you are interested in advertising with Clemson Podcasts please let us know! We offer multiple packages starting at just $100 per month! Select packages include advertising on www.clemsonpodcasts.com, www.tigernet.com, LIVE on Vokle, the podcast additions, or the radio show heard throughout South Carolina. </p>
 
-<p>  Email <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a></p>
-</div></td>
-      <td width="126" align="left" valign="top" class="bgrdRt"><?php require_once('../_includes/inc_google_ads_rt.php'); ?></td>
-    </tr>
-  </table>
+<p align="left">Advertising with Clemson Podcasts can put your company in the eyes and ears of Clemson fans, as well as sports fans around the world. We also have one of the most influential Clemson Twitter feeds on the web. Placing an advertisement with Clemson Podcasts will spread your company's advertisement around the globe.</p>
+<p align="left">Feel free to contact us at <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a> if you have any interest in being a sponsor on our website or programs. We look forward to creating a relationship with your company that can take your product line to the masses.
+  </p>
+</p> 
+<div align="center"><img src="../_images/CP_Flyer.gif" width="557" height="721" /></div>
+<p align="center">  Email: <a href="mailto:clemson81@gmail.com">clemson81@gmail.com</a></p>
+			<p>
+		</div>
+	</div>
+	<div class='unit size1of5 sponsors'>
+		<h2>Sponsors</h2>
+		<?php require_once('../_includes/inc_google_ads_rt.php'); ?>
+	</div>
 </div>
-
-<?php require_once('../_includes/inc_bot.php'); ?>
-<?php require_once('../_includes/inc_footer.php'); ?>
-
-</div>
-
-<?php require_once('../_includes/scripts.php'); ?>
-</body>
-</html>
+<?php endblock() ?>
