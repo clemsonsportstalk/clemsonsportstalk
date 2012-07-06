c209db7222584e711dbfe7a473ce217dab3319af
Brad Dunbar <dunbarb2@gmail.com>
2012-07-06 16:52:05 -0400

    Add advertising section.

--- _includes/inc_donate.php
+++ _includes/inc_donate.php
@@ -1,14 +1,31 @@
 <h2>Social Media</h2>
 <div class='donate pam'>
-          <a href="http://www.facebook.com/clemsonpodcasts"><img src="../_images/Icons/facebook.png" width="30" height="30" longdesc="Facebook" /></a> <a href="http://twitter.com/clemsonpodcasts"><img src="../_images/Icons/twitter.png" width="30" height="30" longdesc="Twitter" /></a> <a href="http://www.google.com/reader/public/atom/user/03107298055788933099/bundle/Clemson Tigers"><img src="../_images/Icons/RSS.png" width="30" height="30" longdesc="RSS" /></a> <a href="http://youtube.com/elswannmedia"><img src="../_images/Icons/you-tube-logo.jpg" width="78" height="30" longdesc="YouTube" /></a>
-	          <p>       
-    <a href='/main/ads.php'>Advertising Information</a>
+  <a href="http://www.facebook.com/clemsonpodcasts">
+    <img src="../_images/Icons/facebook.png" width="30" height="30" longdesc="Facebook" />
+  </a>
+  <a href="http://twitter.com/clemsonpodcasts">
+    <img src="../_images/Icons/twitter.png" width="30" height="30" longdesc="Twitter" />
+  </a>
+  <a href="http://www.google.com/reader/public/atom/user/03107298055788933099/bundle/Clemson Tigers">
+    <img src="../_images/Icons/RSS.png" width="30" height="30" longdesc="RSS" />
+  </a>
+  <a href="http://youtube.com/elswannmedia">
+    <img src="../_images/Icons/you-tube-logo.jpg" width="78" height="30" longdesc="YouTube" />
+  </a>
+</div>
+<h2>Advertising</h2>
+<ul class='links'>
+  <li><a href='/main/ads.php'>Advertising Information</a></li>
+</ul>
+<div class='donate pam'>
 	<form class='pam' action='https://www.paypal.com/cgi-bin/webscr' method='post'>
 		<input type='hidden' name='cmd' value='_s-xclick'>
 		<input type='hidden' name='encrypted' value='-----BEGIN PKCS7-----MIIHLwYJKoZIhvcNAQcEoIIHIDCCBxwCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCQg6UuT2Y0TnZ8xKpElfgcrAlDKhW2hUKAygOjaiVhpMCm2kROc9ouZoeRc9eAX/mCx+BkGCf5K7xBzF67EYd5+lQssCjlL1F1Xp+nnY/EqEJDWwJ8MzR0oMgu8vzobsPuhm7gYM+I+NwWcXcOcxDhz5E4T/c65CJbi0dXICOsDjELMAkGBSsOAwIaBQAwgawGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIl0Q4jkK5ZS+AgYgYDb9ljg7vJHZaRJJ/XIhVWkK56LASpySCMkVJIp8di00A95566AaJ/DqKW6ouMK0FyVYlK9P73+3TY+zztvaPY/wwFUgCrOB1zzsmjdd0xmA1IRGBvWErQycDbXDGhBXuWEvFCtGq+n+u2T8OFA2mqw9AL2txqofiQZaHaALig/y5qBKboaNGoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTAxMTA4MDQ0NjA0WjAjBgkqhkiG9w0BCQQxFgQUd6kvULj2Ou1rMEpfu34dni5NgDIwDQYJKoZIhvcNAQEBBQAEgYBFwm6/s7Qt14Le2fbpkse5NDNyGseAKnyztHujN5U29sy4qELFAeuoWOSQRfMZChDjEiiVo7WwDTUwr1LdheRtHccZwFAzoCe84riE2tkx15yatac0j13khkCXjH25jG/wdKsMvhTZfgeusHXWLCIiUU6T2GxYeM+9izn9sqNgHg==-----END PKCS7-----'>
 		<input type='image' src='https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'>
 		<img border='0' src='https://www.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1'>
   </form>
-    <a href='http://landing.stitcher.com/?vurl=CLEMSONPODCASTS'>
-							<img src="../_images/bug_300x250.jpg" width="174" height="160"></a>
-                            Promo Code: <strong>CLEMSONPODCASTS</strong></div>
+  <a href='http://landing.stitcher.com/?vurl=CLEMSONPODCASTS'>
+    <img src="../_images/bug_300x250.jpg" width="174" height="160">
+  </a>
+  Promo Code: <strong>CLEMSONPODCASTS</strong>
+</div>
